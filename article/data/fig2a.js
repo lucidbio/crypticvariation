5b3aa0b3ca736b4fd28fc92ed272c24f327071df
Theo Sanderson <theosanderson@theosanderson-macbookpro2.roam.corp.google.com>
2019-05-11 14:39:40 -0400

    Refactor Fig2A data to remote file

---
+++ article/data/fig2a.js
@@ -0,0 +1,264 @@
+fig2a = {
+  "visdat": {
+    "9fc2f97716a": ["function () ", "plotlyVisDat"]
+  },
+  "cur_data": "9fc2f97716a",
+  "attrs": {
+    "9fc2f97716a": {
+      "alpha_stroke": 1,
+      "sizes": [10, 100],
+      "spans": [1, 20],
+      "x": {},
+      "y": {},
+      "color": {},
+      "colors": ["#3B469F", "#436CB0", "#28A6D3", "#106134", "#108246", "#9DB750"],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "size": 7
+      },
+      "hoverinfo": "text",
+      "hovertext": {},
+      "inherit": true
+    }
+  },
+  "layout": {
+    "margin": {
+      "b": 40,
+      "l": 60,
+      "t": 25,
+      "r": 10
+    },
+    "xaxis": {
+      "domain": [0, 1],
+      "automargin": true,
+      "title": "Library",
+      "titlefont": {
+        "family": "Arial",
+        "size": 16
+      },
+      "ticks": "outside",
+      "tickvals": [1, 2, 3, 4, 5, 6],
+      "ticktext": ["Lib 1", "Lib 2", "Lib 3", "Lib 1", "Lib 2", "Lib 3"]
+    },
+    "yaxis": {
+      "domain": [0, 1],
+      "automargin": true,
+      "title": "Fold change in fitness (Variant/WT)",
+      "type": "log",
+      "range": [-1, 2],
+      "titlefont": {
+        "family": "Arial",
+        "size": 16
+      },
+      "ticks": "outside"
+    },
+    "showlegend": false,
+    "hovermode": "closest"
+  },
+  "source": "A",
+  "config": {
+    "showSendToCloud": false
+  },
+  "data": [
+    {
+      "x": [0.818942321371287, 0.977036136109382, 0.938338974677026, 1.13386636218056, 0.912993213906884, 0.916095530148596, 0.971162913367152],
+      "y": [55, 45, 40, 25, 2.1, 1.9, 1.6],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "color": "rgba(59,70,159,1)",
+        "size": 7,
+        "line": {
+          "color": "rgba(59,70,159,1)"
+        }
+      },
+      "hoverinfo": ["text", "text", "text", "text", "text", "text", "text"],
+      "hovertext": ["<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 55 <b><br> Mutations: <\/b> W93G (r1)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 45 <b><br> Mutations: <\/b> W93G (r1)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 40 <b><br> Mutations: <\/b> W93G (r1)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 25 <b><br> Mutations: <\/b> W93C (r1), K181Q", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 2.1 <b><br> Mutations: <\/b> M154T (r5), K276R", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.9 <b><br> Mutations: <\/b> M154T (r5), K276R", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.6 <b><br> Mutations: <\/b> M154V (r5)"],
+      "name": "N1",
+      "textfont": {
+        "color": "rgba(59,70,159,1)"
+      },
+      "error_y": {
+        "color": "rgba(59,70,159,1)"
+      },
+      "error_x": {
+        "color": "rgba(59,70,159,1)"
+      },
+      "line": {
+        "color": "rgba(59,70,159,1)"
+      },
+      "xaxis": "x",
+      "yaxis": "y",
+      "frame": null
+    },
+    {
+      "x": [2.07853702874854, 1.8082681604661, 1.89877463756129, 2.05391943119466, 2.0437670789659],
+      "y": [28, 1.7, 1.5, 1.5, 1.5],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "color": "rgba(67,108,176,1)",
+        "size": 7,
+        "line": {
+          "color": "rgba(67,108,176,1)"
+        }
+      },
+      "hoverinfo": ["text", "text", "text", "text", "text"],
+      "hovertext": ["<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 28 <b><br> Mutations: <\/b> W93L (r1), N57D", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.7 <b><br> Mutations: <\/b> V113A, H133R", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.5 <b><br> Mutations: <\/b> V133A, H133R", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.5 <b><br> Mutations: <\/b> V133A, H133R", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.5 <b><br> Mutations: <\/b> M154V (r5), D212N"],
+      "name": "N2",
+      "textfont": {
+        "color": "rgba(67,108,176,1)"
+      },
+      "error_y": {
+        "color": "rgba(67,108,176,1)"
+      },
+      "error_x": {
+        "color": "rgba(67,108,176,1)"
+      },
+      "line": {
+        "color": "rgba(67,108,176,1)"
+      },
+      "xaxis": "x",
+      "yaxis": "y",
+      "frame": null
+    },
+    {
+      "x": [2.89910528054461, 3.14405627893284, 2.92279045246541, 2.83403787873685, 2.82495997613296, 3.09054032303393, 3.06339980373159],
+      "y": [2.7, 2.3, 2.3, 2.3, 2.2, 1.9, 1.6],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "color": "rgba(40,166,211,1)",
+        "size": 7,
+        "line": {
+          "color": "rgba(40,166,211,1)"
+        }
+      },
+      "hoverinfo": ["text", "text", "text", "text", "text", "text", "text"],
+      "hovertext": ["<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 2.7 <b><br> Mutations: <\/b> M154T (r5)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 2.3 <b><br> Mutations: <\/b> M154T (r5)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 2.3 <b><br> Mutations: <\/b> M154T (r5)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 2.3 <b><br> Mutations: <\/b> M154T (r5)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 2.2 <b><br> Mutations: <\/b> M154T (r5)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.9 <b><br> Mutations: <\/b> M154T (r5)", "<b>Ortholog: <\/b> NDM1 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.6 <b><br> Mutations: <\/b> L148F"],
+      "name": "N3",
+      "textfont": {
+        "color": "rgba(40,166,211,1)"
+      },
+      "error_y": {
+        "color": "rgba(40,166,211,1)"
+      },
+      "error_x": {
+        "color": "rgba(40,166,211,1)"
+      },
+      "line": {
+        "color": "rgba(40,166,211,1)"
+      },
+      "xaxis": "x",
+      "yaxis": "y",
+      "frame": null
+    },
+    {
+      "x": [4.03336539771408, 4.16300064185634, 4.15648761829361, 4.00380510017276, 3.85740537904203, 4.08153314031661],
+      "y": [1.4, 1.5, 1.4, 1.4, 1.4, 1.9],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "color": "rgba(16,97,52,1)",
+        "size": 7,
+        "line": {
+          "color": "rgba(16,97,52,1)"
+        }
+      },
+      "hoverinfo": ["text", "text", "text", "text", "text", "text"],
+      "hovertext": ["<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> D68A, Q108H, P241Q, T263A (r5)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.5 <b><br> Mutations: <\/b> V72A (r1)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> V41G (r6), S66P (r9), D68N", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> D68A, Q108H, P241Q, T263A (r5)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> K107T, S202G (r4), E242K", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 1 <b><br> Fold Change Fitness: <\/b> 1.9 <b><br> Mutations: <\/b> V41G (r6), S66P (r9), D68N"],
+      "name": "V1",
+      "textfont": {
+        "color": "rgba(16,97,52,1)"
+      },
+      "error_y": {
+        "color": "rgba(16,97,52,1)"
+      },
+      "error_x": {
+        "color": "rgba(16,97,52,1)"
+      },
+      "line": {
+        "color": "rgba(16,97,52,1)"
+      },
+      "xaxis": "x",
+      "yaxis": "y",
+      "frame": null
+    },
+    {
+      "x": [4.98282756730914, 4.98184862937778, 4.8131390853785, 4.97856609728187, 4.96122409496456, 4.80064513785765, 4.87131808102131, 4.93434692164883],
+      "y": [1.4, 1.6, 1.6, 1.6, 1.4, 1.4, 1.1, 1.3],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "color": "rgba(16,130,70,1)",
+        "size": 7,
+        "line": {
+          "color": "rgba(16,130,70,1)"
+        }
+      },
+      "hoverinfo": ["text", "text", "text", "text", "text", "text", "text", "text"],
+      "hovertext": ["<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> F67L (r2)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.6 <b><br> Mutations: <\/b> F67L (r2)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.6 <b><br> Mutations: <\/b> F67L (r2)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.6 <b><br> Mutations: <\/b> V72A (r1)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> F67L (r2)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> F67L (r2)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.1 <b><br> Mutations: <\/b> Y73F, I79V", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 2 <b><br> Fold Change Fitness: <\/b> 1.3 <b><br> Mutations: <\/b> R50Q, F67L (r2)"],
+      "name": "V2",
+      "textfont": {
+        "color": "rgba(16,130,70,1)"
+      },
+      "error_y": {
+        "color": "rgba(16,130,70,1)"
+      },
+      "error_x": {
+        "color": "rgba(16,130,70,1)"
+      },
+      "line": {
+        "color": "rgba(16,130,70,1)"
+      },
+      "xaxis": "x",
+      "yaxis": "y",
+      "frame": null
+    },
+    {
+      "x": [5.88046797607094, 5.93382776146755, 6.04865929735824, 5.92422757772729, 6.13668640777469, 5.80471160924062, 5.83765708897263],
+      "y": [1.3, 1.4, 0.9, 1, 1.3, 1.1, 1.5],
+      "type": "scatter",
+      "mode": "markers",
+      "marker": {
+        "color": "rgba(157,183,80,1)",
+        "size": 7,
+        "line": {
+          "color": "rgba(157,183,80,1)"
+        }
+      },
+      "hoverinfo": ["text", "text", "text", "text", "text", "text", "text"],
+      "hovertext": ["<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.3 <b><br> Mutations: <\/b> P157S", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.4 <b><br> Mutations: <\/b> S202R (r4)", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 0.9 <b><br> Mutations: <\/b> V131A", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1 <b><br> Mutations: <\/b> D84G", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.3 <b><br> Mutations: <\/b> F67L (r2), I156T", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.1 <b><br> Mutations: <\/b> A100V, S144T", "<b>Ortholog: <\/b> VIM2 <b><br> Library: <\/b> 3 <b><br> Fold Change Fitness: <\/b> 1.5 <b><br> Mutations: <\/b> D68N"],
+      "name": "V3",
+      "textfont": {
+        "color": "rgba(157,183,80,1)"
+      },
+      "error_y": {
+        "color": "rgba(157,183,80,1)"
+      },
+      "error_x": {
+        "color": "rgba(157,183,80,1)"
+      },
+      "line": {
+        "color": "rgba(157,183,80,1)"
+      },
+      "xaxis": "x",
+      "yaxis": "y",
+      "frame": null
+    }
+  ],
+  "highlight": {
+    "on": "plotly_click",
+    "persistent": false,
+    "dynamic": false,
+    "selectize": false,
+    "opacityDim": 0.2,
+    "selected": {
+      "opacity": 1
+    },
+    "debounce": 0
+  },
+  "shinyEvents": ["plotly_hover", "plotly_click", "plotly_selected", "plotly_relayout", "plotly_brushed", "plotly_brushing", "plotly_clickannotation", "plotly_doubleclick", "plotly_deselect", "plotly_afterplot"],
+  "base_url": "https://plot.ly"
+}
